c768c6c06c5180a2aa3b94d9a003aecf06b5cbac
João Pedro Toledo Martins <joaopedrotoledomartins@gmail.com>
2024-04-23 21:47:57 -0300

    imagens2

--- forca/script.js
+++ forca/script.js
@@ -45,17 +45,17 @@ document.getElementById("adivinhar").addEventListener("click", function() {
         tentaAtua.innerHTML = `Tentativas restantes: ${tentativasRestantes}`;
         switch(tentativasRestantes)
         {
-            case 5:document.querySelector("#imagem").src="./Imagens/cabeça.png";
+            case 5:document.querySelector("#imagem").src="/Imagens/cabeça.png";
             break;
-            case 4:document.querySelector("#imagem").src="./Imagens/corpo.png";
+            case 4:document.querySelector("#imagem").src="/Imagens/corpo.png";
             break;
-            case 3:document.querySelector("#imagem").src="./Imagens/perna1.png";
+            case 3:document.querySelector("#imagem").src="/Imagens/perna1.png";
             break;
-            case 2:document.querySelector("#imagem").src="./Imagens/perna2.png";
+            case 2:document.querySelector("#imagem").src="/Imagens/perna2.png";
             break;
-            case 1:document.querySelector("#imagem").src="./Imagens/braço1.png";
+            case 1:document.querySelector("#imagem").src="/Imagens/braço1.png";
             break;
-            case 0:document.querySelector("#imagem").src="./Imagens/braço2.png";
+            case 0:document.querySelector("#imagem").src="/Imagens/braço2.png";
         } 
     }
     // Verificar se o jogador ganhou ou perdeu
