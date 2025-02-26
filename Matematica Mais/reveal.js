ccf110f109bf7c3f487bb55e03adc2ba6fd0813c
João Pedro Toledo Martins <joaopedrotoledomartins@gmail.com>
2025-02-25 21:20:51 -0300

    Alterações visuais: scroll reveal e animações

---
+++ Matematica Mais/reveal.js
@@ -0,0 +1,112 @@
+window.revelar = ScrollReveal({reset:true});
+
+//TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo-do-site',
+{
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-img-topo-do-site',
+{
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+});
+
+//TÍTULOS DAS SESSÕES
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+});
+
+//SESSÃO DE OBJETIVOS
+revelar.reveal('.efeito-objetivo-1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+});
+revelar.reveal('.efeito-objetivo-2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+});
+revelar.reveal('.efeito-objetivo-3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+});
+
+//SESSÃO SOBRE
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'top'
+});
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+
+//SESSÃO CONTEÚDOS
+revelar.reveal('.efeito-txt-conteudo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-link-conteudo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+});
+
+//SESÃO FORMULÁRIO
+revelar.reveal('.efeito-input-form-1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-input-form-2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1250,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-input-form-3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-input-form-4',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1750,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-input-form-5',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000,
+    origin: 'bottom'
+});
