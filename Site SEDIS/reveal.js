230e35d2829ba474dcaa7e4a8bd66bbff2371e7b
João Pedro Toledo Martins <joaopedrotoledomartins@gmail.com>
2025-03-08 19:27:48 -0300

    Novo site da SEDIS: 1ª versão

---
+++ Site SEDIS/reveal.js
@@ -0,0 +1,116 @@
+window.revelar = ScrollReveal({reset:false});
+
+//TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo-do-site',
+{
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-img-topo-do-site',
+{
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+});
+
+//TÍTULOS DAS SESSÕES
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+});
+
+//SESSÃO DE OBJETIVOS
+revelar.reveal('.efeito-objetivo-1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+});
+revelar.reveal('.efeito-objetivo-2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+});
+revelar.reveal('.efeito-objetivo-3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+});
+
+//SESSÃO SOBRE
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'top'
+});
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+
+//SESSÃO CONTEÚDOS
+revelar.reveal('.efeito-txt-integrantes',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+revelar.reveal('.efeito-link-integrantes',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+});
+
+//SESSÃO FORMULÁRIO
+revelar.reveal('#efeito-label-form-1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+});
+
+revelar.reveal('.efeito-input-form-1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1200,
+    origin: 'bottom'
+});
+
+revelar.reveal('#efeito-label-form-2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1250,
+    origin: 'bottom'
+});
+
+revelar.reveal('.efeito-input-form-2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1450,
+    origin: 'bottom'
+});
+
+revelar.reveal('.efeito-input-form-3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1700,
+    origin: 'bottom'
+});
